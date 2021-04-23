5f6e29d4c4932965c74ef2f63ad9f3c820358331
kwameasare04 <73545744+kwameasare04@users.noreply.github.com>
2021-04-23 12:16:37 +0100

    Create task1.js

---
+++ task1.js
@@ -0,0 +1,27 @@
+/*
+Modify parameters section in the "processQuantities" function to match console.log outputs
+*/
+//Old Code!
+// const processQuantities = (/* parameters */) => {
+//   console.log(minQty); // 8
+//   console.log(maxQty); // 29
+//   console.log(defaultQty); // 0
+//   return maxQty - minQty; // returns 21
+// };
+
+// const qtyRange = [8, 29];
+
+// processQuantities(qtyRange);
+
+
+//Modified code
+const processQuantities = ([minQty, maxQty, defaultQty = 0]) => {
+  console.log(minQty); // 8
+  console.log(maxQty); // 29
+  console.log(defaultQty); // 0
+  return maxQty - minQty; // returns 21
+};
+
+const qtyRange = [8, 29];
+
+console.log(processQuantities(qtyRange));
